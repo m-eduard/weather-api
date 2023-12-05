8028659110d762e1e3bff2c71c3636ca6e01673c
m-eduard <marineduardconstantin2001@gmail.com>
2023-12-06 01:28:10 +0200

    Add list of dependencies to condition the current element insertion on other values which should be already stored in different collections

--- weather/src/db_utils.py
+++ weather/src/db_utils.py
@@ -23,13 +23,34 @@ def check_uniqueness(
 
 
 def insert_if_unique(
-    collection: collection.Collection, document: dict, unique_fields: list
+    collection: collection.Collection,
+    document: dict,
+    unique_fields: list,
+    dependencies: list = [],
 ) -> Union[results.InsertOneResult, None]:
     if check_uniqueness(
         collection, unique_fields, [document[field] for field in unique_fields]
     ):
-        document["_id"] = get_next_index(
-            collection.database.get_collection("counters"), collection.name
-        )
-        return collection.insert_one(document)
+        valid_insert = True
+
+        for dependency in dependencies:
+            # Check that for all the fields in the dependency, the value associated
+            # with each field can be found in the dependency collection
+            dependency_solved = collection.database.get_collection(
+                dependency["collection"]
+            ).find_one(
+                filter={dependency["destField"]: document[dependency["srcField"]]},
+            )
+
+            if not dependency_solved:
+                valid_insert = False
+                break
+
+        if valid_insert:
+            document["_id"] = get_next_index(
+                collection.database.get_collection("counters"), collection.name
+            )
+            return collection.insert_one(document)
+        else:
+            raise Exception(f"Dependency {dependency} could not be solved")
     return None
