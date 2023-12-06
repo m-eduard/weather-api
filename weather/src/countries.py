45dd9dffc020e62cbf7a02b07bc5199797af8026
m-eduard <marineduardconstantin2001@gmail.com>
2023-12-06 17:25:16 +0200

    Add more verbose JSON exception response for duplicate insertion

--- weather/src/countries.py
+++ weather/src/countries.py
@@ -7,6 +7,9 @@ from validators import Operations, unique_fields, validation_error, validators
 
 with current_app.app_context():
     mongo_client = MongoClient(current_app.config["mongo_uri"])
+    db = mongo_client[os.environ.get("DB_NAME")]
+    collection = db.get_collection("countries")
+
     api_countries = Blueprint("api_countries", __name__)
 
     @api_countries.route("/api/countries/", methods=["POST"])
@@ -15,18 +18,28 @@ with current_app.app_context():
         if not validators[Operations.POST_COUNTRY](body):
             return Response(validation_error(Operations.POST_COUNTRY), status=400)
 
-        db = mongo_client[os.environ.get("DB_NAME")]
-        collection = db.get_collection("countries")
-
         try:
             new_country = db_utils.insert_if_unique(
                 collection, body, unique_fields[Operations.POST_COUNTRY]
             )
 
             # Throw an error if a country with the same name already exists
-            new_country.inserted_id
-        except:
-            return Response(f"Adding a new country, {body}, failed in DB", status=409)
+            if not new_country:
+                raise Exception(
+                    f"Another country with the same {unique_fields[Operations.POST_CITY]} already exists in {collection.name}"
+                )
+        except Exception as e:
+            return Response(
+                str(
+                    {
+                        "operation": Operations.POST_COUNTRY.value,
+                        "collection": collection.name,
+                        "body": body,
+                        "error": e,
+                    }
+                ),
+                status=409,
+            )
 
         response = jsonify({"id": new_country.inserted_id})
         response.status_code = 201
