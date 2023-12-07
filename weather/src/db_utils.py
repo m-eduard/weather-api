42c95e3c88114572d0cafde5169ce9e859f2667d
m-eduard <marineduardconstantin2001@gmail.com>
2023-12-07 21:33:38 +0200

    Add support for update operation in check_uniqueness and implement update

--- weather/src/db_utils.py
+++ weather/src/db_utils.py
@@ -1,6 +1,8 @@
-from typing import Union
+import datetime as dt
+from typing import Tuple, Union
 
-from pymongo import ReturnDocument, collection, results
+from bson.timestamp import Timestamp
+from pymongo import ReturnDocument, collection, database, results
 
 
 def get_next_index(counters: collection.Collection, counter_type: str) -> int:
@@ -14,12 +16,40 @@ def get_next_index(counters: collection.Collection, counter_type: str) -> int:
 
 
 def check_uniqueness(
-    collection: collection.Collection, fields: list, values: list
+    collection: collection.Collection,
+    fields: list,
+    values: list,
+    update: Tuple[bool, int] = (False, 0),
 ) -> bool:
-    return (
-        collection.find_one({field: value for (field, value) in zip(fields, values)})
-        is None
+    query_res = collection.find_one(
+        {field: value for (field, value) in zip(fields, values)}
     )
+
+    # When we want to update the document with the specified _id,
+    # then we should ignore its old data when checking for uniqueness
+    if update[0]:
+        return query_res is None or query_res["_id"] == update[1]
+    return query_res is None
+
+
+# Function that tries to solve all the dependencies
+# for a subset of fields for a given document, and stops
+# at the first unsolvable dependency
+def solve_dependencies(
+    database: database.Database,
+    document: dict,
+    dependencies: list = [],
+) -> Tuple[bool, dict]:
+    for dependency in dependencies:
+        # Check that for all the fields in the dependency, the value associated
+        # with each field can be found in the dependency collection
+        dependency_solved = database.get_collection(dependency["collection"]).find_one(
+            filter={dependency["destField"]: document[dependency["srcField"]]},
+        )
+
+        if not dependency_solved:
+            return False, dependency
+    return True, {}
 
 
 def insert_if_unique(
@@ -27,24 +57,17 @@ def insert_if_unique(
     document: dict,
     unique_fields: list,
     dependencies: list = [],
+    timestamp: bool = False,
 ) -> Union[results.InsertOneResult, None]:
+    if timestamp:
+        document["timestamp"] = Timestamp(int(dt.datetime.utcnow().timestamp()), 1)
+
     if check_uniqueness(
         collection, unique_fields, [document[field] for field in unique_fields]
     ):
-        valid_insert = True
-
-        for dependency in dependencies:
-            # Check that for all the fields in the dependency, the value associated
-            # with each field can be found in the dependency collection
-            dependency_solved = collection.database.get_collection(
-                dependency["collection"]
-            ).find_one(
-                filter={dependency["destField"]: document[dependency["srcField"]]},
-            )
-
-            if not dependency_solved:
-                valid_insert = False
-                break
+        valid_insert, unsolved_dependency = solve_dependencies(
+            collection.database, document, dependencies
+        )
 
         if valid_insert:
             document["_id"] = get_next_index(
@@ -52,5 +75,33 @@ def insert_if_unique(
             )
             return collection.insert_one(document)
         else:
-            raise Exception(f"Dependency {dependency} could not be solved")
+            raise Exception(f"Dependency {unsolved_dependency} could not be solved")
+    return None
+
+
+def update(
+    collection: collection.Collection,
+    filter: dict,
+    document: dict,
+    unique_fields: list,
+    dependencies: list = [],
+    timestamp: bool = False,
+) -> Union[results.UpdateResult, None]:
+    if timestamp:
+        document["timestamp"] = Timestamp(int(dt.datetime.utcnow().timestamp()), 1)
+
+    if check_uniqueness(
+        collection,
+        unique_fields,
+        [document[field] for field in unique_fields],
+        update=(True, document["_id"]),
+    ):
+        valid_insert, unsolved_dependency = solve_dependencies(
+            collection.database, document, dependencies
+        )
+
+        if valid_insert:
+            return collection.update_one(filter, {"$set": document})
+        else:
+            raise Exception(f"Dependency {unsolved_dependency} could not be solved")
     return None
