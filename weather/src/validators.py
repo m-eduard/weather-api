a6bb7fb3409846e992c33991f875a67080f2e584
m-eduard <marineduardconstantin2001@gmail.com>
2023-12-07 21:39:25 +0200

    Update validators to check fields for PUT country and also map JSON body field names to DB names

--- weather/src/validators.py
+++ weather/src/validators.py
@@ -6,6 +6,8 @@ class Operations(Enum):
     POST_COUNTRY = "post_country"
     POST_CITY = "post_city"
     POST_TEMPERATURE = "post_temperature"
+    GET_COUNTRIES = "get_countries"
+    PUT_COUNTRY = "put_country"
 
 
 def _validate_body(body: Any, types: Dict[str, type]) -> bool:
@@ -29,12 +31,19 @@ validators = {
     Operations.POST_TEMPERATURE: lambda body: _validate_body(
         body, expected_types[Operations.POST_TEMPERATURE]
     ),
+    Operations.PUT_COUNTRY: lambda body: _validate_body(
+        body, expected_types[Operations.PUT_COUNTRY]
+    ),
 }
 
 expected_types = {
     Operations.POST_COUNTRY: {"nume": str, "lat": float, "lon": float},
     Operations.POST_CITY: {"idTara": int, "nume": str, "lat": float, "lon": float},
     Operations.POST_TEMPERATURE: {"idOras": int, "valoare": float},
+}
+expected_types[Operations.PUT_COUNTRY] = {
+    **expected_types[Operations.POST_COUNTRY],
+    **{"id": int},
 }
 
 unique_fields = {
@@ -46,11 +55,34 @@ unique_fields = {
 dependencies = {
     Operations.POST_CITY: [
         {"collection": "countries", "srcField": "idTara", "destField": "_id"}
-    ]
+    ],
+    Operations.POST_TEMPERATURE: [
+        {"collection": "cities", "srcField": "idOras", "destField": "_id"}
+    ],
 }
-
 
 validation_error = lambda op: (
     "Expected fields: "
     + f"{', '.join(str((x[0], x[1].__name__)) for x in expected_types[op].items())}"
 )
+
+# Dict used to map the field names from the document stored in DB to the
+# ones that are expected by the client from our API
+api_response_field_mappings = {
+    Operations.GET_COUNTRIES: {
+        "_id": "id",
+    }
+}
+
+# Dict used to map the field names from the request body to the ones used in DB
+api_request_field_mappings = {
+    Operations.PUT_COUNTRY: {
+        "id": "_id",
+    }
+}
+
+
+def map_fields(document: dict, field_mappings: dict) -> dict:
+    return {
+        field_mappings[k] if k in field_mappings else k: v for k, v in document.items()
+    }
