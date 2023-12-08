66d294ae23dfb1cf894f845bb906f2c8b3c863aa
m-eduard <marineduardconstantin2001@gmail.com>
2023-12-08 20:20:44 +0200

    Add elegant implementation for temperatures POST

--- weather/src/temperatures.py
+++ weather/src/temperatures.py
@@ -1,5 +1,6 @@
 import os
 
+import api_utils
 import db_utils
 from flask import Blueprint, Response, current_app, jsonify, request
 from pymongo import MongoClient
@@ -21,10 +22,13 @@ with current_app.app_context():
     @api_temperatures.route("/api/temperatures/", methods=["POST"])
     def post_temperature():
         body = request.get_json(silent=False)
-        if not validators[Operations.POST_TEMPERATURE](body):
-            return Response(validation_error(Operations.POST_TEMPERATURE), status=400)
 
         try:
+            if not validators[Operations.POST_TEMPERATURE](body):
+                raise api_utils.ValidationError(
+                    validation_error(Operations.POST_TEMPERATURE)
+                )
+
             new_temperature = db_utils.insert_if_unique(
                 collection,
                 body,
@@ -32,26 +36,14 @@ with current_app.app_context():
                 dependencies[Operations.POST_TEMPERATURE],
                 timestamp=True,
             )
-
-            # Throw an error if a city with the same name and country id already exists
-            if not new_temperature:
-                raise Exception(
-                    f"Another temperature with the same {unique_fields[Operations.POST_TEMPERATURE]} already exists in {collection.name}"
-                )
-        except Exception as e:
-            return Response(
-                str(
-                    {
-                        "operation": Operations.POST_TEMPERATURE.value,
-                        "collection": collection.name,
-                        "body": body,
-                        "error": e,
-                    }
-                ),
-                status=409,
+        except (
+            api_utils.ValidationError,
+            api_utils.ResourceDependencyError,
+            api_utils.DuplicateResourceError,
+        ) as e:
+            error_message = api_utils.build_error_message(
+                Operations.POST_TEMPERATURE, collection.name, body, e
             )
+            return api_utils.exception_handlers[type(e)](error_message)
 
-        response = jsonify({"id": new_temperature.inserted_id})
-        response.status_code = 201
-
-        return response
+        return jsonify({"id": new_temperature.inserted_id}), 201
